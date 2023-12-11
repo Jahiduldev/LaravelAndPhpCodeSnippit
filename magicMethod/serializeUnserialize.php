4c71273cdf10c6c99741bf628e4e174c33d59648
Jahidul Islam <jahiduldev@gmail.com>
2023-12-11 16:53:48 +0600

    call method

--- magicMethod/serializeUnserialize.php
+++ magicMethod/serializeUnserialize.php
@@ -1,13 +1,15 @@
 <?php
+
 class Person
 {
-
     public $name = "Jahidul";
     public $age = 36;
-
 }
 
 $person = new Person();
 $serialize = serialize($person);
-$unSerialize = unserialize($person);
-var_dump($serialize);
+
+// Unserialize the serialized string
+$unserializedPerson = unserialize($serialize);
+
+var_dump($unserializedPerson);
