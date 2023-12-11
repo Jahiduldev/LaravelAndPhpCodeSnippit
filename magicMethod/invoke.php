98f672d366a3bb9f76a6cecf1d164200d61d706b
Jahidul Islam <jahiduldev@gmail.com>
2023-12-11 16:36:00 +0600

    call method

--- magicMethod/invoke.php
+++ magicMethod/invoke.php
@@ -1,21 +1,10 @@
 <?php
 class Person
 {
-
-    public $name = "Jahidul";
-    public $age = 36;
-
-
     public function __invoke()
     {
        return "Object is called as a function";
     }
-
-    public function getMyNameAndAge()
-    {
-        return "My name".$this->name."Age".$this->age;
-    }
-
 }
 
 $person = new Person();
