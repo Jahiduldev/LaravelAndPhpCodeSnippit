5f396ab4dd4d62b1055cea4e6a3f1b2800ac7984
Jahidul Islam <jahiduldev@gmail.com>
2023-12-11 16:57:04 +0600

    sleep and wakeup method

---
+++ magicMethod/sleepWakeUp.php
@@ -0,0 +1,28 @@
+<?php
+class MyClass {
+public $data1 = 'Some data 1';
+public $data2 = 'Some data 2';
+public $data3 = 'Some data 3';
+public $data4 = 'Some data 4';
+
+public function __sleep() {
+// Return an array of property names to be serialized
+return ['data1', 'data2'];
+}
+
+public function __wakeup() {
+// Any post-unserialization logic can be placed here
+$this->data3 = 'Restored data 3';
+$this->data4 = 'Restored data 4';
+}
+}
+
+$obj = new MyClass();
+
+// Serialize the object - only 'data1' and 'data2' will be serialized
+$serialized = serialize($obj);
+
+// Unserialize the serialized data
+$unserialized = unserialize($serialized);
+
+var_dump($unserialized);
