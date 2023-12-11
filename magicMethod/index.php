08a6ed95cf7f3a6a9edc2230ba6cc1491728fbeb
Jahidul Islam <jahiduldev@gmail.com>
2023-12-11 16:12:33 +0600

    magic method

---
+++ magicMethod/index.php
@@ -0,0 +1,24 @@
+<?php
+
+Class Person
+{
+    public $age = 35;
+    public $phone = "02598453";
+    public function __construct()
+    {
+        echo "Construct is called". PHP_EOL;
+    }
+
+    public function __destruct()
+    {
+        echo "Construct is called". PHP_EOL;
+    }
+
+    public function __toString()
+    {
+        return  "Age". $this->age. PHP_EOL."Phone". $this->phone;
+    }
+}
+
+$obj = new Person("dd");
+echo $obj;
